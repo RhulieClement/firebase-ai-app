84a3a1db401849fa8da87befbffbcec8081329ce
sandylulu928 <sandylulu928@gmail.com>
2025-01-24 09:31:24 +0000

    first commit

---
+++ src/environments/environments.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  firebase: {
+      apiKey: 'AIzaSyB8ebmHHEc8fKeUg4_AwsOwlS-tKEdsauo',
+      authDomain: 'fir-ai-app-ca3c5.firebaseapp.com',
+      databaseURL: '',
+      projectId: 'fir-ai-app-ca3c5',
+      storageBucket: 'fir-ai-app-ca3c5.firebasestorage.app',
+      messagingSenderId: '614068368350',
+      appId: '1:614068368350:web:07a6df8992005ac395234f',
+      measurementId: '',
+  },
+  gemini_api_key: 'AIzaSyCLFGJMn5FVargZ_omhLyZ5wq792cZV-Ic',
+  debug_token: ''
+};
